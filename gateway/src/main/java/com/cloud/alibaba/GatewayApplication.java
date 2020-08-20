aece1e5f08a8baed446d91feeb6c0efa83a23846
八两 <332857067@qq.com>
2020-08-20 15:35:45 +0800

    使用nacos进行动态配置，多文件配置

--- gateway/src/main/java/com/cloud/alibaba/GatewayApplication.java
+++ gateway/src/main/java/com/cloud/alibaba/GatewayApplication.java
@@ -1,24 +1,14 @@
 package com.cloud.alibaba;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class GatewayApplication {
 
-    public static void main(String[] args) throws InterruptedException {
-        ConfigurableApplicationContext applicationContext=SpringApplication.run(GatewayApplication.class, args);
-        while (true){
-            System.out.println(applicationContext.getEnvironment().getProperty("msg"));
-            Thread.sleep(2000);
-        }
-
+    public static void main(String[] args){
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(GatewayApplication.class, args);
+        System.out.println(applicationContext.getEnvironment().getProperty("msg"));
     }
 }
